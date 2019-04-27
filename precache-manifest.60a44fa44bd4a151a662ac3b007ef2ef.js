d836cbc1e87c3a1083d93692235cc2ea7e4948b2
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2019-04-27 23:32:08 +0200

    Updates

---
+++ precache-manifest.60a44fa44bd4a151a662ac3b007ef2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb31b0007323ab37426a25d58b4bb2d0",
+    "url": "/finding-islands/index.html"
+  },
+  {
+    "revision": "da5ec6f6553029d44678",
+    "url": "/finding-islands/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d413a2f7d1778a1dc15d",
+    "url": "/finding-islands/static/css/main.cbd8aceb.chunk.css"
+  },
+  {
+    "revision": "da5ec6f6553029d44678",
+    "url": "/finding-islands/static/js/2.7be55751.chunk.js"
+  },
+  {
+    "revision": "d413a2f7d1778a1dc15d",
+    "url": "/finding-islands/static/js/main.5dc8bade.chunk.js"
+  },
+  {
+    "revision": "e284b2593bbcbedf2307",
+    "url": "/finding-islands/static/js/runtime~main.9999872c.js"
+  }
+]);
